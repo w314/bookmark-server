e2100b6b5b3ccbbf91213d3069c4302411023199
Piroska Werner <piroska.werner@gmail.com>
2019-01-29 16:02:45 -0600

    Use PORT from enviroment

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -44,6 +44,7 @@
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
+import os
 
 memory = {}
 
@@ -161,6 +162,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT',8000)) #use PORT if it's there
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
 web: python BookmarkServer.py
-
-
